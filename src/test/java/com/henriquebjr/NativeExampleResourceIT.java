3d7d24dba13c76dcf8e334c3fcae6c2241c25bce
Henrique Luiz Bona Junior <henrique.bona@fluig.com>
2020-11-21 13:31:09 -0300

    project scaffolding

---
+++ src/test/java/com/henriquebjr/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package com.henriquebjr;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
